25fc77f7d3d093f91ba571ed4875c4379faf61c5
KenPaolaRod <kenpaorin@gmail.com>
2024-02-05 18:36:48 -0500

    testing images for deploy

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,8 +4,8 @@ function Skills() {
   return (
     <div className='skills-container'>
       <h2>Skills</h2>
-      <img src="../../public/img/Figma-1-logo.png" alt="Figma Logo" />
-      <img src="../../public/img/HTML5_logo.svg.png" alt="HTML Logo" />
+      <img src="./public/img/Figma-1-logo.png" alt="Figma Logo" />
+      <img src=" img/HTML5_logo.svg.png" alt="HTML Logo" />
       <img src="../../public/img/css3-logo-png-transparent.png" alt="CSS Logo" />
       <img src="../../public/img/javascript-logo.png" alt="Javascript Logo" />
       <img src="../../public/img/React-logo.png" alt="HTML Logo" />
